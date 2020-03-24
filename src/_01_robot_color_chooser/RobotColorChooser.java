5cccc12f63069970b6dc827da954198005d6df7d
root <root@3a746d9ad09e>
2020-03-24 21:33:42 +0000

    <RobotColorChooser>

--- src/_01_robot_color_chooser/RobotColorChooser.java
+++ src/_01_robot_color_chooser/RobotColorChooser.java
@@ -4,24 +4,44 @@
 package _01_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+		Robot Siri = new Robot(); 
 		//3. Ask the user what color they would like the robot to draw
-		
-		//5. Use an if/else statement to set the pen color that the user requested
 
+		//5. Use an if/else statement to set the pen color that the user requested
+for (int i = 0; i <4; i++) {
+	String color =JOptionPane.showInputDialog("What color do you want?(Green, Red, or Blue)");	
+
+	if (color == "Red") {
+		Siri.setPenColor(250,0,0);
+	} else {
+		if (color == "Green") {
+			Siri.setPenColor(0,250,0);
+	
+		} else {
+			if (color == "Blue") {
+				Siri.setPenColor(0,0,250);
+			} else {
+				Siri.setRandomPenColor();
+			}
+		}
+	} 
         //6. If the user doesn’t enter anything, choose a random color
 
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
 		//4. Set the pen width to 10
-		
+		Siri.setPenWidth(10);
+		Siri.penDown();
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-
-
+		Siri.turn(90);
+		Siri.move(100);
+}
 	}
 }
